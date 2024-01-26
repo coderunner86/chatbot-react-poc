13da2f047d0c7361470a3d4fd7a8e038aa4e3203
coderunner86 <java@unicauca.edu.com>
2024-01-26 12:25:09 -0500

    class3: Add funcional components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className='bg-black text-white mini-container'>
+        <h2>React Component</h2>
+      </div>
+    </React.Fragment>
+  )
+}
+function TextContent() {
+  const changeColor = () => {
+    document.getElementById('myText').style.background = '#FF0000';
+  };
+
+  return (
+    <React.Fragment>
+      <p className="learn-react-beginner" id="myText">Function component in React</p>
+      <button className="btn btn-success" id="submit-btn" onClick={changeColor}>Change color</button>
+    </React.Fragment>
   );
 }
-
+function MainContent() {
+  return (
+    <React.Fragment>
+      <div className="all-content">
+        <TextContent />
+      </div>
+    </React.Fragment>
+  );
+}
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <div className='bg-black text-white mini-container'>
+      </div>
+    </React.Fragment>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      {<Header />}
+      {<MainContent />}
+      {<Footer />}
+    </div>
+  );
+};
 export default App;
